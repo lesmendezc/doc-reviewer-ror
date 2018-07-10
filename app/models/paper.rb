a819acfcecc7cbd1b5f84b8736261357ce090f84
Lesly Méndez C <leslicita_mc@hotmail.com>
2018-07-10 15:03:39 -0400

    Change flash error messages and language

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -2,12 +2,12 @@ class Paper < ApplicationRecord
     has_one_attached :pdf_file
     acts_as_taggable
 
-    validate :correct_file
+    validates :title, presence: { message: " - es un campo obligatorio" }
 
-    def correct_file
-        if pdf_file.attached? && !pdf_file.attachment.blob.content_type.in?(%w(application/pdf))
-            errors.add(:pdf_file, 'Debe ser un archivo PDF')
-        end
+    validate :exists_file?
+
+    private
+    def exists_file?
+        errors.add(:base, 'Debe elegir un archivo PDF') unless pdf_file.attached?
     end
-    
 end
