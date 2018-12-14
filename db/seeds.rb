212379d0522f913fa41b3508cce5b58c3d69966c
Lesly Méndez C <leslicita_mc@hotmail.com>
2018-12-14 00:49:29 -0400

    seeds increment, question options

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,13 @@
 User.create([{name: 'Juan Pablo', lastname: 'Sandoval Alcocer', role: 'admin', email: 'sandoval@ucb.edu.bo', career: 'Ingeniería de Sistemas', password: '.sandoval.', username: 'sandoval', professor_role: true},
              {name: 'Tatiana', lastname: 'Aparicio', role: 'docente', email: 'aparicio@ucb.edu.bo', career: 'Ingeniería de Sistemas', password: 'password', username: 'tatiana', professor_role: true},
              {name: 'Javier', lastname: 'Vildozo', role: 'estudiante', email: 'javier@vildozo.bo', career: 'Ingeniería de Sistemas', password: 'password', username: 'javier', professor_role: false}])
-event = Event.create({name: 'Primera Revisión Perfil', year: '2018', career: 'Ingeniería de Sistemas'})
+
+event = Event.create({name: 'Primera Revisión Perfil', year: '2018', career: 'Ingeniería de Sistemas', limit_date: Date.new(2018,12,18)})
 survey = Survey.create({name: 'Perfil en fase #1', event_id: event.id})
-selectOptionQuestion = Questions::SelectOption.create({description: 'Introducción', survey_id: survey.id})
-textQuestion = Questions::Text.create({description: 'Antecedentes', survey_id: survey.id})
+selectOptionQuestion = Questions::SelectOption.create({ description: 'Introducción', survey_id: survey.id,
+    options_attributes: [ { point: 1, description: 'Bueno' },
+        { point: 2, description: 'Regular'},
+        { point: 3, description: 'Malo'} ]})
+textQuestion = Questions::Text.create({description: 'Antecedentes', title: 'Se debe redactar con el estándar APA', survey_id: survey.id})
+
+event = Event.create({name: 'Segunda Revisión Perfil', year: '2018', career: 'Ingeniería de Sistemas', limit_date: Date.new(2018,12,27)})
