ec1101b73e897f1975c0da68a405e03f13d2d57a
Lesly Méndez C <leslicita_mc@hotmail.com>
2018-12-16 12:13:39 -0400

    change labels and tags on survey form

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,5 +2,5 @@ class Survey < ApplicationRecord
     has_many :questions
     has_many :reviews
     belongs_to :event
-    accepts_nested_attributes_for :questions, allow_destroy: true, reject_if: lambda { |q| q[:description].blank? }
+    accepts_nested_attributes_for :questions, allow_destroy: true, reject_if: lambda { |q| q[:title].blank? }
 end
