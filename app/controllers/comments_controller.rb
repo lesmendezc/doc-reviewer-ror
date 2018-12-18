34b0e3d960043f75a0c5d70ad2f9c121633fb363
Lesly Méndez C <leslicita_mc@hotmail.com>
2018-12-18 03:08:16 -0400

    redirect to paper reviews on comment create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,7 +15,7 @@ class CommentsController < ApplicationController
         @event = Event.find(params[:event_id])
         @paper = @event.papers.find(params[:paper_id])
         @comment = @paper.comments.create(comment_params)
-        redirect_to event_paper_comments_path(@event)
+        redirect_to event_paper_reviews_url(@event, @paper)
     end
 
     def comment_params
