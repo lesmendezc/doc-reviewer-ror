2c781b325fd1d4f9ca76826bdbaff30bf886373f
Lesly Méndez C <leslicita_mc@hotmail.com>
2018-12-18 03:14:25 -0400

    test surveys questions max_score sums 100

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -7,15 +7,24 @@ class Survey < ApplicationRecord
 
     validates_presence_of :name
     validates_associated :questions
-    validate :number_of_questions
+    validate :at_least_one_question
+    validate :maximum_score_is_hundred
 
     def quantitative_questions
         questions.select { |question| question.is_quantitative? }
     end
+    
+    def maximum_score
+        questions.sum(:max_score)
+    end
 
     private 
 
-    def number_of_questions
+    def at_least_one_question
         errors.add(:questions, "should be at least 1") if questions.size < 1
+    end
+
+    def maximum_score_is_hundred
+        errors.add(:questions, "maximum scores should sum 100") if questions.sum(:max_score) != 100
     end
 end
--- spec/models/survey_spec.rb
+++ spec/models/survey_spec.rb
@@ -17,4 +17,14 @@ describe Survey do
         expect(@survey.valid?).to be_falsy
     end
 
+    it 'it is valid when the maximum scores of its questions sum 100' do
+        expect(@survey.maximum_score).to be == 100
+        expect(@survey.valid?).to be_truthy
+    end
+
+    it 'it is invalid the maximum scores of its questions do not sum 100' do
+        @survey.quantitative_questions.each {|question| question.max_score = 1}
+        @survey.save
+        expect(@survey.valid?).to be_falsy
+    end
 end
