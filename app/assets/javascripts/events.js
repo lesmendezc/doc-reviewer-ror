d14c5decc9ea76ef03d9afecf3d77c8dd052eb43
Lesly Méndez C <leslicita_mc@hotmail.com>
2018-12-25 22:25:36 -0400

    validations in event form

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,12 +1,21 @@
 $(function() {
+    $.validator.setDefaults({
+        ignore:[]
+      });
     $('#event_form').validate({
         rules: {
-            'event[name]': 'required' 
+            'event[name]': 'required',
+            'event[semester]': 'required',
+            'event[career]': 'required',
+            'event[limit_date]': 'required'
         },
         messages: { 
-            'event[name]': 'El nombre no puede estar vacío'
+            'event[name]': 'El nombre no puede estar vacío',
+            'event[semester]': 'Debe elegir un semestre',
+            'event[career]': 'Debe elegir la carrera',
+            'event[limit_date]': 'Debe elegir una fecha límite'
         },
-        errorElement : 'div',
+        errorElement: 'div',
         errorPlacement: function(error, element) {
             error.insertAfter(element);
         }
