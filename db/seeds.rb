4b289fc5a221a63f31f78b9fbfc9e522aac70e5e
Lesly Méndez C <leslicita_mc@hotmail.com>
2019-01-13 13:52:50 -0400

    added data to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,11 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create([{name: 'Juan Pablo', lastname: 'Sandóval Alcocer', professional_degree: 'Dr.', role: 'admin', email: 'sandoval@ucb.edu.bo', career: 'Ingeniería de Sistemas', password: '.sandoval.', username: 'sandoval', professor_role: true},
-             {name: 'Yanina', lastname: 'Galaburda', professional_degree: 'Mgr.', role: 'director', email: 'yanina@ucb.edu.bo', career: 'Ingeniería de Sistemas', password: 'yanina', username: 'yanina'},
+             {name: 'Yanina', lastname: 'Galaburda', professional_degree: 'Mgr.', role: 'director', email: 'yanina@ucb.edu.bo', career: 'Ingeniería de Sistemas', password: 'galaburda', username: 'yanina'},
              {name: 'Tatiana', lastname: 'Aparicio', professional_degree: 'Mgr.', role: 'docente', email: 'aparicio@ucb.edu.bo', career: 'Ingeniería de Sistemas', password: 'aparicio', username: 'tatiana'},
-             {name: 'Lesly', lastname: 'Méndez Cruz', role: 'estudiante', email: 'mendez@gmail.com', career: 'Ingeniería de Sistemas', password: 'mendez', username: 'lesmendezc'}])
+             {name: 'Ismael', lastname: 'Delgado', professional_degree: 'Mgr.', role: 'docente', email: 'ismael@delgado.com', career: 'Ingeniería de Sistemas', password: 'delgado', username: 'ismael'},
+             {name: 'Wendoline', lastname: 'Arteaga', professional_degree: 'Mgr.', role: 'docente', email: 'wendo@arteaga.com', career: 'Ingeniería de Sistemas', password: 'arteaga', username: 'wendo'},
+             {name: 'Lesly', lastname: 'Méndez Cruz', role: 'estudiante', email: 'mendez@gmail.com', career: 'Ingeniería de Sistemas', password: 'mendez', username: 'lesmendezc'},
+             {name: 'Estela', lastname: 'Herbas', professional_degree: 'Mgr.', role: 'director', email: 'estela@herbas.com', career: 'Ingeniería Ambiental', password: 'herbas', username: 'estela'}])
 
 event = Event.create({name: 'Primera Revisión Perfil', year: '2019', semester: 'Primer', career: 'Ingeniería de Sistemas', limit_date: Date.new(2019,01,31)})
