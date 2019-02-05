ed58b3f0c3c6d2cae06544f814ba62708d8d7725
Lesly Méndez C <leslicita_mc@hotmail.com>
2019-02-05 11:59:42 -0400

    fixed bug: register and edit phone in user with maxlength

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -5,6 +5,7 @@ $(function(){
             'user[lastname]': 'required',
             'user[username]': 'required',
             'user[career]': 'required',
+            'user[phone]': { maxlength: 8 },
             'user[password]': {
                 required: true,
                 minlength: 6
@@ -20,6 +21,7 @@ $(function(){
             'user[lastname]': 'Los apellidos no pueden estar vacíos',
             'user[username]': 'El nombre de usuario no puede estar vacío',
             'user[career]': 'Debe elegir una carrera',
+            'user[phone]': { maxlength: 'El celular no puede tener más de 8 caracteres' },
             'user[password]': {
                 required: 'Ingrese su contraseña',
                 minlength: 'La contraseña debe tener al menos 6 caracteres'
@@ -42,14 +44,16 @@ $(function(){
             'user[lastname]': 'required',
             'user[username]': 'required',
             'user[career]': 'required',
-            'user[email]': 'required'
+            'user[email]': 'required',
+            'user[phone]': { maxlength: 8 }
         },
         messages: {
             'user[name]': 'El nombre no puede estar vacío',
             'user[lastname]': 'Los apellidos no pueden estar vacíos',
             'user[username]': 'El nombre de usuario no puede estar vacío',
             'user[career]': 'Debe elegir una carrera',
-            'user[email]': 'El correo no puede estar vacío'
+            'user[email]': 'El correo no puede estar vacío',
+            'user[phone]': { maxlength: 'El celular no puede tener más de 8 caracteres' }
         },
         errorElement: 'div',
         errorPlacement: function(error, element){
